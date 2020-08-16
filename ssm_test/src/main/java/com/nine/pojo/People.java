625a0263f7d0265a00b600860b1d6cce46f45a5f
Huanghj1995 <hhj3517@163.com>
2020-08-16 16:57:34 +0800

    增加email

--- ssm_test/src/main/java/com/nine/pojo/People.java
+++ ssm_test/src/main/java/com/nine/pojo/People.java
@@ -17,7 +17,7 @@ public class People {
         private String name;
         //密码
         private String pwd;
-        
+        private String email;
         public int getId() {
             return id;
         }
@@ -41,7 +41,15 @@ public class People {
         public void setPwd(String pwd) {
             this.pwd = pwd;
         }
-        
+    
+        public String getEmail() {
+            return email;
+        }
+    
+        public void setEmail(String email) {
+            this.email = email;
+        }
+    
         @Override
         public String toString() {
             return "User{" +
